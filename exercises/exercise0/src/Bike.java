e121bcc319a387f2c14a28f3967850fb1f9aa489
hongji <shihongji21@gmail.com>
2023-02-16 19:58:27 -0800

    add enumeration in Bike class

--- exercises/exercise0/src/Bike.java
+++ exercises/exercise0/src/Bike.java
@@ -1,13 +1,43 @@
 public class Bike {
+    public enum State {
+        WORKING,
+        NEEDS_REPAIR,
+        BEING_REPAIRED
+    }
     private int numberOfSpeedLevel;
-    private double weight;
+    private State state;
     private String type;
     private double currentSpeed;
-    private int currentState;
+    private double weight;
     private double radiusOfTyre;
     private String material;
     private boolean isLubricated;
 
+    public void setState(State state) {
+        this.state = state;
+    }
+    public void takeToRepairShop() {
+        if (state == State.WORKING) {
+            state = State.NEEDS_REPAIR;
+            currentSpeed = 0;
+            System.out.println("It needs repair now!");
+        }
+    }
+    public void repair() {
+        if (state == State.NEEDS_REPAIR) {
+            state = State.BEING_REPAIRED;
+            System.out.println("It's being repaired, now return.");
+        }
+    }
+    public void returnFromRepairShop() {
+        if (state ==State.BEING_REPAIRED) {
+            state = State.WORKING;
+            System.out.println("It's brand new now.");
+        }
+    }
+    public State getState() {
+        return state;
+    }
     public void acc() {
         this.currentSpeed +=1;
     }
@@ -18,6 +48,7 @@ public class Bike {
         this.isLubricated = true;
     }
     public Bike(int numberOfSpeedLevel) {
+        this.state = State.WORKING;
         this.numberOfSpeedLevel = numberOfSpeedLevel;
         System.out.println("This is a " +this.numberOfSpeedLevel + " bike, lightening speed!");
     }
--- exercises/exercise0/src/Main.java
+++ exercises/exercise0/src/Main.java
@@ -3,5 +3,11 @@ public class Main {
         Blog blog = new Blog("Google", "Hongji");
         PS5 ps5 = new PS5("DRID387674", "White");
         Song newSong = new Song("The last of us", "HBO", 560);
+        Bike bike = new Bike(2233482);
+        System.out.println("The initial state: " + bike.getState());
+        bike.takeToRepairShop();
+        System.out.println("Now state: "+bike.getState());
+        bike.repair();
+        bike.returnFromRepairShop();
     }
 }
