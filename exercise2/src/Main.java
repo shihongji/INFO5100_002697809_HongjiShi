9a19020bf2cf6ef9c3725140979a72fc749b5f48
hongji <shihongji21@gmail.com>
2023-02-23 10:55:38 -0800

    Start exercise2

---
+++ exercise2/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        //TODO Create a class hierarchy for base class Shape and derived classes Triangle, Rectangle, Circle and Square
+        //TODO Demonstrate concepts of abstraction, overriding, polymorphism and static field
+        // May use methods such as 'calculateArea()', 'calculatePerimeter()', etc. as needed for demonstrating concepts
+        // May use static field to represent color of the class or name of the class to represent concept of static fields
+    }
+}
